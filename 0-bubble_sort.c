e54ba27232a22b4fbf8f9d3e5f6a1ac441249f3b
Alem Haile <welma0043@gmail.com>
2022-07-21 23:23:30 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,14 +8,13 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
-	int temp;
-	int swapped;
+	int temp, swap;
 
 	if (array == NULL)
 		return;
 
 	do {
-		swapped = 0;
+		swap = 0;
 		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1]) /* do the swap */
@@ -23,9 +22,9 @@ void bubble_sort(int *array, size_t size)
 				temp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
-				swapped = 1;
+				swap = 1;
 				print_array(array, size);
 			}
 		}
-	} while (swapped != 0);
+	} while (swap != 0);
 }
